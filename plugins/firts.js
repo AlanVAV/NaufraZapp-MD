17e830753f5af0b0eab3d428ec3e6defe6f608ba
AlanVAV <168609441+AlanVAV@users.noreply.github.com>
2024-05-02 11:44:38 -0300

    Update firts.js

--- plugins/firts.js
+++ plugins/firts.js
@@ -7,18 +7,7 @@ if (m.chat.endsWith('broadcast') || m.fromMe || m.isGroup) return
 let user = global.db.data.users[m.sender]
 
 if (new Date() - user.pc < 21600000) return
-await m.reply(`👋 Hola ${username}¡!
- *${saludo}*
-
-*🚩 Nota:* No Envié Spam Al Bot!
-📇 Escriba !menu Para Ver Los Comandos Que Dispongo!
-
-*💧 Si Tienes Duda O Sugerencia Contacta A Mi Creador: 💚*
-💖 +57 301 2482597 💜
-
-*🧿 Escribe !grupos Para Ver Los Grupos Oficiales Del Bot 🔮*
-
-_🌼 ¿Quieres Apoyar En El Proyecto Del Bot Para Que Siempre Tenga Actualizaciones? Puedes Apoyarnos Con Una Estrellita🌟 Al Repositorio._`)
+await m.reply(`Hola usuario/a, bienvenido/a.`)
 
 user.pc = new Date * 1
 }
