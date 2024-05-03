a218c803375ac358f271541d35c848e3436e1a47
AlanVAV <168609441+AlanVAV@users.noreply.github.com>
2024-05-03 00:40:41 -0300

    Update info-Colaboradores.js

--- plugins/info-Colaboradores.js
+++ plugins/info-Colaboradores.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPrems}) => {
   if (usedPrefix == 'a' || usedPrefix == 'A') return;
   try {
-    const pp = imagen1;
+    const pp = menusImgs4;
     // let vn = './media/menu.mp3'
     const img = './Menu.png';
     const d = new Date(new Date + 3600000);
@@ -21,50 +21,17 @@ const handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, is
     const taguser = '@' + m.sender.split('@s.whatsapp.net')[0];
     const doc = ['pdf', 'zip', 'vnd.openxmlformats-officedocument.presentationml.presentation', 'vnd.openxmlformats-officedocument.spreadsheetml.sheet', 'vnd.openxmlformats-officedocument.wordprocessingml.document'];
 
- await conn.sendMessage(m.chat, { react: { text: '🧩', key: m.key } })
+ await conn.sendMessage(m.chat, { react: { text: '💯', key: m.key } })
 
 
 const document = doc[Math.floor(Math.random() * doc.length)];
- const str = `╭✨️⸽⃕𝕾𝖆𝖐𝖚𝖗𝖆-𝕭𝖔𝖙-𝕷𝖎𝖙𝖊-𝕸𝕯🍁⃨፝⃕✰*
-┃➸ 𝕾𝖆𝖐𝖚𝖗𝖆-𝕭𝖔𝖙-𝕷𝖎𝖙𝖊-𝕸𝕯🧸
-┃➸ 📍𝖢𝗈𝗅𝖺𝖻𝗈𝗋𝖺𝖽𝗈𝗋𝖾𝗌
-╰◆━━━━━━◆❃◆━━━━━━◆
+ const str = `> \`CREADOR DEL BOT:\`
++51 929 838 430
 
-*╭🧸⸽⃕𝗥𝗲𝗽𝗼𝘀𝗶𝘁𝗼𝗿𝗶𝗼𝘀 𝗚𝗶𝘁𝗵𝘂𝗯📍✰*
-┃✣➸ 𝖣𝗂𝖾𝗀𝗈:
-┃ https://github.com/diegojadibot
-┃
-┃✣➸ 𝖠𝗓𝖺𝗆𝗂:
-┃ https://github.com/AzamiJs
-┃
-┃✣➸ Ender Tally:
-┃https://github.com/Ender-GB-Isis777
-┃
-┃✣➸ 𝖣𝖺𝗇𝗂𝖾𝗅:
-┃https://github.com/MoonContentCreator
-┃
-┃✣➸ 𝖬𝖺𝗋𝗂𝗈:
-┃ https://github.com/elrebelde21
-┃
-╰◆━━━━━━◆❃◆━━━━━━◆
-
-╭🧸⸽⃕𝗡𝘂𝗺𝗲𝗿𝗼𝘀 - 𝗖𝗿𝗲𝗮𝗱𝗼𝗿𝗲𝘀📍✰ 
-┋➸ 𝖣𝗂𝖾𝗀𝗈: 
-┋@573012482597
-┋
-┋➸ 𝖠𝗓𝖺𝗆𝗂: 
-┋@5217294888993
-┋
-┋➸ Ender Tally:
-┋@50558124470
-┋
-┋➸ 𝖣𝖺𝗇𝗂𝖾𝗅:
-┋@33760536110
-┋
-┋➸ 𝖬𝖺𝗋𝗂𝗈: 
-┋@5492266613038
-┋
-╰◈ ━━━━━━━ ⸙ ━━━━━━━`.trim();     if (m.isGroup) { 
+> \`COLABORADORES DEL BOT:\`
++52 314 218 3828
++54 9 3873 68-7620
+`.trim();     if (m.isGroup) { 
  // await conn.sendFile(m.chat, vn, 'menu.mp3', null, m, true, { type: 'audioMessage', ptt: true})
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
       conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: m});
@@ -74,7 +41,7 @@ const document = doc[Math.floor(Math.random() * doc.length)];
       conn.sendMessage(m.chat, {image: pp, caption: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net')}, {quoted: fkontak2});
     }
   } catch {
-    conn.reply(m.chat, '*😻 𝘓𝘰𝘴𝘪𝘦𝘯𝘵𝘰, 𝘌𝘳𝘳𝘰𝘳, 𝘕𝘰 𝘗𝘶𝘦𝘥𝘰 𝘌𝘯𝘷𝘪𝘢𝘳 𝘌𝘭 𝘔𝘦𝘯𝘴𝘢𝘫𝘦!*', m);
+    conn.reply(m.chat, '❌ Ocurrio un error inesperado, intentalo de nuevo.', m);
   }
 };
 handler.command = /^(colaboradores|Colaboradores)$/i;
@@ -87,4 +54,5 @@ function clockString(ms) {
   const m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
   const s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
   return [h, m, s].map((v) => v.toString().padStart(2, 0)).join(':');
-}
+                                                                 }
+    
