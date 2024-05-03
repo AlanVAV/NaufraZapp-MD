2aeeae8d045858b5b503e6e28840418554365394
AlanVAV <168609441+AlanVAV@users.noreply.github.com>
2024-05-03 12:13:29 -0300

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,28 +1,22 @@
 let media = './Menu7.png'
 let handler = async (m, { conn, command }) => {
 let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
-    await conn.sendMessage(m.chat, { react: { text: '📇', key: m.key } })
-let str = `*🔮 GRUPOS OFICIALES*
+    await conn.sendMessage(m.chat, { react: { text: '👀', key: m.key } })
+let str = `*💥 GRUPOS OFICIALES🐦‍🔥*
 
-    *_╭━━━⊜ SakuraBotLite-MD_*
-  *_┃🌩❏ ${gp1}_*
-*_╰━━━━━━━━━━━━━━━━⊜_*
+    *Naufrazapp oficial_*
+  *❏ https://chat.whatsapp.com/IxdHYBEnUJ5Eiu3qiDjCTal*
+*_Unete_*
 
- *🔮 GRUPOS EN COLABORACIÓN*
+    *_grupo bot Naufrazapp-MD_*
+  *_❏ https://chat.whatsapp.com/Elot5C62z0AGvxwZjXsW4j*
+*_Unete_*
 
-    *_╭━━━⊜ Curiosity y SakuraBot_*
-  *_┃🌦❏ ${gp2}_*
-*_╰━━━━━━━━━━━━━━━━⊜_*
+  *💥 CANAL OFICIAL🐦‍🔥*
 
-    *_╭━━━⊜ NovaBot & SakuraBot_*
-  *_┃🌦❏ ${gp3}_*
-*_╰━━━━━━━━━━━━━━━━⊜_*
-
-  *🔮 CANAL OFICIAL*
-
-    *_╭━━━⊜ Canal SakuraLite_*
-  *_┃🌦❏ ${channel}_*
-*_╰━━━━━━━━━━━━━━━━⊜_*`
+    *_╭━━━⊜ Canal NaufraZapp-MD_*
+  *❏ ${channel}*
+*_Unete_*`
 await conn.sendFile(m.chat, media, 'gata.mp4', str, fkontak)}
 
 handler.command = /^gruponz|linknz|grupoofc|grupooficial$/i
