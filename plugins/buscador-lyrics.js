c580040f80ef8d5925ea562742cd3b39e71b2ded
AlanVAV <168609441+AlanVAV@users.noreply.github.com>
2024-05-04 18:19:23 -0300

    Update buscador-lyrics.js

--- plugins/buscador-lyrics.js
+++ plugins/buscador-lyrics.js
@@ -19,9 +19,9 @@ img = json.thumbnail.genius;
 const bochil = await googleImage(`${result[0].artist} ${result[0].title}`);
 img = await bochil.getRandom();
 }}
-await conn.sendButton(m.chat, `*TITULO: ${result[0].title || ''}*\n\n*ARTISTA: ${result[0].artist || ''}*\n\n*CANCION:*\n${lyrics || ''}`, null, img, [
+await conn.sendButton(m.chat, `*TITULO: ${result[0].title || ''}*\n\n*ARTISTA: ${result[0].artist || ''}*\n\n*LETRA:*\n${lyrics || ''}`, null, img, [
 ['🎵 𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙧 𝘼𝙪𝙙𝙞𝙤 🎵', `/playdoc ${text}`],
-['🎬 𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙧 𝙑𝙞𝙙𝙚𝙤 🎬', `#playdoc2`],
+['🎬 𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙧 𝙑𝙞𝙙𝙚𝙤 🎬', `#playdoc2 ${text}`],
 ['🌃 𝙈𝙚𝙣𝙪 🌃', '/menu']], null, null, m)
 //conn.sendFile(m.chat, img, 'letra.jpg', `ღ ${mid.smsYT1} :\n💚 *${result[0].title || ''}*\n\nღ ${mid.smsYT2} :\n💜 *${result[0].artist || ''}*\n\nღ ${mid.smsYT3} :\n${lyrics || ''}`, fkontak, false, { contextInfo: {externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, title: gt, body: ' 😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 - 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 ', previewType: 0, thumbnail: gataImg, sourceUrl: accountsgb }}})
 await conn.sendMessage(m.chat, {audio: {url: result[0].preview}, fileName: `${result[0].artist} ${result[0].title}.mp3`, mimetype: 'audio/mp4'}, {quoted: m});
