1c2fb4983b9ecff47094fdd9815198f299c71786
AlanVAV <168609441+AlanVAV@users.noreply.github.com>
2024-05-04 18:39:27 -0300

    Update owner-banchat.js

--- plugins/owner-banchat.js
+++ plugins/owner-banchat.js
@@ -1,6 +1,6 @@
 const handler = async (m) => {
   global.db.data.chats[m.chat].isBanned = true;
-  m.reply('✅️ *Chat Baneado Con Exito, Ya No Podran Usar SakuraBotLite - MD*');
+  m.reply('✅ 𝙀𝙡 𝙘𝙝𝙖𝙩 𝙛𝙪𝙚 𝙗𝙖𝙣𝙚𝙖𝙙𝙤 𝙘𝙤𝙣 𝙚𝙭𝙞𝙩𝙤.');
 };
 handler.help = ['banchat'];
 handler.tags = ['owner'];
