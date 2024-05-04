d925138cf85c4bf125e39487ceb6b06278ecec85
AlanVAV <168609441+AlanVAV@users.noreply.github.com>
2024-05-04 19:00:32 -0300

    Update karolina.js

--- plugins/karolina.js
+++ plugins/karolina.js
@@ -1,13 +1,14 @@
-let media = menusImgs4.getRandom()
+let media = imagen4
 let handler = async (m, { conn, command }) => {
 let user = db.data.users[m.sender]
 let str = `📁 𝙎𝙀𝙇𝙀𝘾𝘾𝙄𝙊𝙉𝙀 𝙀𝙇 𝙈𝙊𝘿𝙊 𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍.
 `
 await conn.sendButton(m.chat, str, `Ajustes para grupos.\n` + author, media, [
-['👥 Banear chat', '.banchat'],
-['⏱️ Cerrar por 1H', '#grouptime on 1'],
-['👥 Cerrar el grupo', '/grupo cerrar']], null, [
+['✅ Abrir el grupo', '.grupo abrir'],
+['⛔ Cerrar el grupo', '#grupo cerrar'],
+['🗂️ Configuración', '/funcs']], null, [
 ['Canal', `${channel}`]], m)}
-handler.command = /^ajgrupo$/i
+handler.command = /^grupo|group$/i
 handler.exp = 80
 export default handler
+
