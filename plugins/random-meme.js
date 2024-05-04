6f3b869809b71a33adec6a72d17422f1f181e9ab
AlanVAV <168609441+AlanVAV@users.noreply.github.com>
2024-05-04 18:32:32 -0300

    Update random-meme.js

--- plugins/random-meme.js
+++ plugins/random-meme.js
@@ -1,12 +1,11 @@
 /*   Creditos - ig : https://www.instagram.com/fg98._/    */
-import hispamemes from 'hispamemes';
-const handler = async (m, {command, conn}) => {
-  const url = await hispamemes.meme();
-  conn.sendFile(m.chat, url, 'error.jpg', `*『✦』Meme*`, m);
-};
-// conn.sendButton(m.chat, `_${command}_`.trim(), author, url, [['🔄 𝚂𝙸𝙶𝚄𝙸𝙴𝙽𝚃𝙴 🔄', `/${command}`]], m)}
-handler.help = ['meme'];
-handler.tags = ['random'];
-handler.command = /^(meme|memes)$/i;
-handler.register = true
-export default handler;
+import hispamemes from 'hispamemes'
+let handler = async (m, {command, conn}) => {
+const url = await hispamemes.meme()  
+//conn.sendFile(m.chat, url, 'error.jpg', `😂😂😂`, m)}
+conn.sendButton(m.chat, `_🤣 ${command}_`.trim(), wm, url, [['↩️ 𝑶𝑻𝑹𝑶 ↩️', `/${command}`]], null, null, m)}
+handler.help = ['meme']
+handler.tags = ['random']
+handler.command = /^(meme|memes)$/i
+export default handler
+
