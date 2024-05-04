25b5bda2982b4c8c7c5bec6f7d95bdbe1ce29c58
AlanVAV <168609441+AlanVAV@users.noreply.github.com>
2024-05-04 19:01:22 -0300

    Update karolina.js

--- plugins/karolina.js
+++ plugins/karolina.js
@@ -4,8 +4,8 @@ let user = db.data.users[m.sender]
 let str = `📁 𝙎𝙀𝙇𝙀𝘾𝘾𝙄𝙊𝙉𝙀 𝙀𝙇 𝙈𝙊𝘿𝙊 𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍.
 `
 await conn.sendButton(m.chat, str, `Ajustes para grupos.\n` + author, media, [
-['✅ Abrir el grupo', '.grupo abrir'],
-['⛔ Cerrar el grupo', '#grupo cerrar'],
+['✅ Abrir el grupo', '.nfzgrupo abrir'],
+['⛔ Cerrar el grupo', '#nfzgrupo cerrar'],
 ['🗂️ Configuración', '/funcs']], null, [
 ['Canal', `${channel}`]], m)}
 handler.command = /^grupo|group$/i
