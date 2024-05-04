1f9a78ddea1990693c5e0623a1d2a20f706ab20b
AlanVAV <168609441+AlanVAV@users.noreply.github.com>
2024-05-04 07:05:17 -0300

    Update button-prueba.js

--- plugins/button-prueba.js
+++ plugins/button-prueba.js
@@ -1,6 +1,6 @@
 const handler = async (m, {conn}) => {
   try {
-    const pp = imagen4;
+    const thumbnail = imagen4;
     const img = await(await fetch('https://images3.alphacoders.com/125/1251707.png')).buffer();
     const _uptime = process.uptime() * 1000;
     const uptime = clockString(_uptime);
@@ -12,7 +12,7 @@ const handler = async (m, {conn}) => {
     } else {
       const fkontak2 = {'key': {'participants': '0@s.whatsapp.net', 'remoteJid': 'status@broadcast', 'fromMe': false, 'id': 'Halo'}, 'message': {'contactMessage': {'vcard': `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}, 'participant': '0@s.whatsapp.net'};
       //conn.sendMessage(m.chat, {text: str.trim(), mentions: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), contextInfo: {forwardingScore: 9999999, isForwarded: true, mentionedJid: [...str.matchAll(/@([0-9]{5,16}|0)/g)].map((v) => v[1] + '@s.whatsapp.net'), "externalAdReply": {"showAdAttribution": true, "containsAutoReply": true, "renderLargerThumbnail": true, "title": global.titulowm, "containsAutoReply": true, "mediaType": 1, "thumbnail": pp, "mediaUrl": `https://github.com/Diego-YL-177/Yotsuba-Nakano-MD`, "sourceUrl": `https://github.com/Diego-YL-177/Yotsuba-Nakano-MD`}}}, {quoted: fkontak2});
-     await conn.sendButton(m.chat, str, wm, pp, [
+     await conn.sendButton(m.chat, str, wm, thumbnail, [
     ['Creador', `${usedPrefix}creador`],
     ['Comprar bot', `${usedPrefix}installbot`]
   ], null, [['Canal', `${channel}`]], m)
