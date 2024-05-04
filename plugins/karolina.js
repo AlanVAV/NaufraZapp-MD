8629b1687234017212a820c1e1efd6d35fcb712e
AlanVAV <168609441+AlanVAV@users.noreply.github.com>
2024-05-04 18:48:47 -0300

    Update karolina.js

--- plugins/karolina.js
+++ plugins/karolina.js
@@ -1,56 +1,13 @@
-let handler = async (m, { isPrems, conn }) => {
-let time = global.db.data.users[m.sender].lastcofre + 0 // 36000000 10 Horas //86400000 24 Horas
-if (new Date - global.db.data.users[m.sender].lastcofre < 0) throw `[❗𝐈𝐍𝐅𝐎❗] 𝚈𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚂𝚃𝙴 𝚃𝚄 𝙲𝙾𝙵𝚁𝙴\𝚗𝚅𝚄𝙴𝙻𝚅𝙴 𝙴𝙽 *${msToTime(time - new Date())}* 𝙿𝙰𝚁𝙰 𝚅𝙾𝙻𝚅𝙴𝚁 𝙰 𝚁𝙴𝙲𝙻𝙰𝙼𝙰𝚁`
-
-let img = 'https://telegra.ph/file/39103fc7092ca4228d253.png'
-let dia = Math.floor(Math.random() * 30)
-let tok = Math.floor(Math.random() * 10)
-let hadesb = Math.floor(Math.random() * 4000)
-let expp = Math.floor(Math.random() * 5000)
-
-  global.db.data.users[m.sender].limit += dia
-  global.db.data.users[m.sender].money += hadesb
-  global.db.data.users[m.sender].joincount += tok
-  global.db.data.users[m.sender].exp += expp
-
-let texto = `*KarolG La Bichota!!😻💖*`
-
-const fkontak = {
-        "key": {
-    "participants":"0@s.whatsapp.net",
-                "remoteJid": "status@broadcast",
-                "fromMe": false,
-                "id": "Halo"
-        },
-        "message": {
-                "contactMessage": {
-                        "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-                }
-        },
-        "participant": "0@s.whatsapp.net"
-}
-await conn.sendFile(m.chat, img, 'hades.jpg', texto, fkontak)
-//await conn.sendButton(m.chat, texto, wm, img, [['🔰 𝙼𝙴𝙽𝚄', '/menu'] ], fkontak, m)  
-global.db.data.users[m.sender].lastcofre = new Date * 1
-}
-handler.help = ['daily']
-handler.tags = ['xp']
-handler.command = ['karolg', 'carolina', 'karol'] 
-handler.register = true
+let media = menusImgs4.getRandom()
+let handler = async (m, { conn, command }) => {
+let user = db.data.users[m.sender]
+let str = `📁 𝙎𝙀𝙇𝙀𝘾𝘾𝙄𝙊𝙉𝙀 𝙀𝙇 𝙈𝙊𝘿𝙊 𝙋𝙊𝙍 𝙁𝘼𝙑𝙊𝙍.
+`
+await conn.sendButton(m.chat, str, `Ajustes para grupos.\n` + author, media, [
+['👥 Banear chat', '.banchat'],
+['⏱️ Cerrar por 1H', '#grouptime on 1'],
+['👥 Cerrar el grupo', '/grupo cerrar']], null, [
+['Canal', `${channel}`]], fkontak)}
+handler.command = /^ajgrupo$/i
+handler.exp = 80
 export default handler
-
-function pickRandom(list) {
-return list[Math.floor(Math.random() * list.length)]}
-
-function msToTime(duration) {
-  var milliseconds = parseInt((duration % 1000) / 100),
-    seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
-
-  hours = (hours < 10) ? "0" + hours : hours
-  minutes = (minutes < 10) ? "0" + minutes : minutes
-  seconds = (seconds < 10) ? "0" + seconds : seconds
-
-  return hours + " Horas " + minutes + " Minutos"
-}
